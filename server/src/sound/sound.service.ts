a71070780197f31158eb0a0240fde88daace80c4
Lucas Moesch <lucasmoesch@live.de>
2022-09-01 23:22:34 +0200

    WIP

--- server/src/sound/sound.service.ts
+++ server/src/sound/sound.service.ts
@@ -1,11 +1,4 @@
-
-import * as fs from 'fs';
-import  * as Speaker from 'speaker-arm64';
-
 import { Injectable } from '@nestjs/common';
-
-
-// import * as player from 'play-sound'
 
 @Injectable()
 export class SoundService {
@@ -15,42 +8,19 @@ export class SoundService {
     
     private player = require('play-sound')({});
 
-
-    // constructor() {
-    
-    // }
-
-    private speakerConfig = {
-        device: "hw1,0",
-        channels: 2,          // 2 channels
-        bitDepth: 16,         // 16-bit samples
-        sampleRate: 44100     // 44,100 Hz sample rate
+    constructor() {
+        this.playWav('assets/wav/triggered/blackout.wav');
     }
-
 
 
     public async playWav(file: string): Promise<void> {
         return new Promise((resolve, reject) => {
-            this.player.play(file);
-            resolve();
-            // if (!this.isPlaying && !this.isMuted) {
-            //     const stream = fs.createReadStream(file);
-            //     const speaker = new Speaker(this.speakerConfig);
-
-            //     this.isPlaying = true;
-            //     speaker.on('finish', () => {
-            //         this.isPlaying = false;
-            //         resolve(); 
-            //     })
-            //     speaker.on('error', (err) => {
-            //         console.log(err);
-            //     })
-            //     stream.pipe(speaker)
-            // }
-            // else {
-            //     // reject();
-            // }
-            this
+            const p = this.player.play(file, console.log);
+            console.log(p)
+            p.on('exit', () => {
+                console.log("???")
+                resolve();
+            })
         })
     }
 }
